970d6ca86d332347e0d4d2e1687951ab826eca65
徐顺钞 <fire330327@vip.qq.com>
2020-07-02 16:57:45 +0800

    project init

---
+++ src/main/java/per/xusc/qsm/QuartzSchedulerManagerApplication.java
@@ -0,0 +1,13 @@
+package per.xusc.qsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzSchedulerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/per/xusc/qsm/QuartzSchedulerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package per.xusc.qsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSchedulerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
